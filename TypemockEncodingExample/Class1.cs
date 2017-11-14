b9440708f1bd7bfb799876a27e6eb5b55802db94
Ryan Rodemoyer <ryan.rodemoyer@mortgagecadence.com>
2017-11-14 15:05:36 -0700

    initial commit

---
+++ TypemockEncodingExample/Class1.cs
@@ -0,0 +1,121 @@
+﻿using System.Text;
+using NUnit.Framework;
+using TypeMock;
+using TypeMock.ArrangeActAssert;
+
+namespace TypemockEncodingExample
+{
+    public static class SomeInterface_Old
+    {
+        public static void SomeDataTransferMethod(string data)
+        {
+            var encoder = new ASCIIEncoding();
+            byte[] ascii = encoder.GetBytes(data);
+
+            // pretend we send out this data to some web service or interface, etc.
+            // SendDataToAnotherWebService(ascii);
+        }
+    }
+
+    public static class SomeInterface1
+    {
+        /// <summary>
+        /// This implementation is **not** testable via Typemock, or any other mocking provider
+        /// </summary>
+        /// <param name="data"></param>
+        public static void SomeDataTransferMethod(string data)
+        {
+            byte[] utf8 = Encoding.UTF8.GetBytes(data);
+
+            // pretend we send out this data to some web service or interface, etc.
+        }
+    }
+
+    public static class SomeInterface2
+    {
+        public static void SomeDataTransferMethod(string data)
+        {
+            byte[] utf8 = new CompanyNameUTF8Encoder().GetBytes(data);
+
+            // pretend we send out this data to some web service or interface, etc.
+        }
+    }
+
+    /// <summary>
+    /// Proxy for System.Text.Encoding.UTF8.
+    /// </summary>
+    /// <remarks>
+    /// Useful so that we can write tests around our code that encodes/decodes UTF8 data.
+    /// </remarks>
+    public class CompanyNameUTF8Encoder
+    {
+        public byte[] GetBytes(string data)
+        {
+            return Encoding.UTF8.GetBytes(data);
+        }
+    }
+
+    [TestFixture, Isolated]
+    public class SomeInterfaces_Tests
+    {
+        [Test]
+        public void faking_types_from_mscorlib_results_in_TypemockException()
+        {
+            // arrange
+            Assert.Throws<TypeMockException>(() => Isolate.WhenCalled(() => Encoding.UTF8.GetBytes(default(string))).CallOriginal());
+
+            // act and assert are commented because the test is actually over @ line 17.
+            // but I'm leaving these below to show you what the test would look like if we could fake Encoding.UTF8.GetBytes(string).
+
+            // act
+            //SomeInterface1.SomeDataTransferMethod("my name is!");
+
+            // assert
+            //Isolate.Verify.WasCalledWithExactArguments(() => Encoding.UTF8.GetBytes("my name is!"));
+        }
+
+        [Test]
+        public void test_failure()
+        {
+            // arrange
+            Isolate.WhenCalled(() => Encoding.UTF8.GetBytes(default(string))).CallOriginal();            
+        }
+
+        /// <summary>
+        /// Note that we are **not** testing the encoder here, but verifying our Code Under Test simply uses the encoder
+        /// so we know that the data is getting encoded via UTF8.
+        /// </summary>
+        [Test]
+        public void verify_CompanyNameUTF8Encoder_is_used_by_our_interface_calls()
+        {
+            // arrange
+            CompanyNameUTF8Encoder encoder = Isolate.Swap.NextInstance<CompanyNameUTF8Encoder>().WithRecursiveFake();
+            
+            // act
+            SomeInterface2.SomeDataTransferMethod("my name is!");
+
+            // assert
+            Isolate.Verify.WasCalledWithExactArguments(() => encoder.GetBytes("my name is!"));
+        }
+    }
+
+    [TestFixture]
+    public class CompanyNameUTF8Encoder_Tests
+    {
+        [TestCase("")]
+        [TestCase("    ")]
+        [TestCase("my name is!")]
+        public void encoder_calls_to_System_Text_Encoding_GetBytes(string data)
+        {
+            // arrange
+            byte[] expected = Encoding.UTF8.GetBytes(data);
+
+            // act
+            var encoder = new CompanyNameUTF8Encoder();
+            byte[] actual = encoder.GetBytes(data);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ TypemockEncodingExample/README.md
@@ -0,0 +1,41 @@
+# Unfakeable Types and Updating Code
+
+## Agenda
+
+1. **mscorlib**  
+Fakeable types via Typemock (below) or experience a TypemockException.
+1. **Gist.**  
+http://bit.ly/2ieJbl1
+1. Scenario.
+
+### Scenario
+We need to make a small update within a much larger set of functionality:
+1. **How do we test that change?**  
+Test the smallest slice of functionality as possible.
+1. **How do we make that change?**  
+Write a test that verifies the behavior or confirms the bug, then make updates.
+
+ ## Typemock and mscorlib
+http://www.typemock.com/mscorlib-types 
+
+Typemock Isolator supports the following types within mscorlib:
+
+* System
+	* DateTime
+	* Environment
+* System.IO
+	* File
+	* Directory
+	* FileStream
+* System.Security.Cryptography.X509Certificates
+	* X509Certificate
+
+## Exception with unfakeable types
+TypeMock.TypeMockException : 
+*** Exception throw while recording calls. Please check:
+ * Are you trying to fake a field instead of a property? try to set field or use Isolate.Invoke.StaticConstructor
+ * Are you are trying to fake an unsupported mscorlib method? See supported types here: http://www.typemock.com/mscorlib-types 
+InnerException
+  ----> System.Reflection.TargetInvocationException : Exception has been thrown by the target of an invocation.
+  ----> System.ArgumentNullException : String reference not set to an instance of a String.
+Parameter name: s
